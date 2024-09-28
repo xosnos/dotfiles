0606fc5a29abf1e3efa609b88c94d4ddc52f9f80
Steven Nguyen <stevennguyen1021@outlook.com>
2024-09-28 11:53:13 -0700

    Add homebrew.sh script

---
+++ homebrew.sh
@@ -0,0 +1,36 @@
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/xosnos/.zprofile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Package Managers
+# brew install node
+# brew install yarn
+# brew install python3
+# brew install git
+
+# Terminal
+# brew install gh
+# brew install watchman
+# brew install fastlane
+# brew install cocoapods
+# brew install openjdk
+# sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+
+# Casks
+# brew install --cask google-chrome
+# brew install --cask spotify
+# brew install --cask vnc-viewer
+# brew install --cask visual-studio-code
+# brew install --cask jetbrains-toolbox
+# brew install --cask notion
+# brew install --cask rectangle
+# brew install --cask topnotch
+# brew install --cask fig
+# brew install --cask bootstrap-studio
+# brew install --cask github
+# brew install --cask discord
+# brew install --cask obsidian
+# brew install --cask alt-tab
+# brew install --cask expo-orbit
+# brew install --cask amazon-q
+# brew install --cask iterm2
